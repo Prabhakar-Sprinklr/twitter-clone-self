2e1d692ca9864e70412ae6e0921b939e19f25403
Prabhakar Kumar <prabhakar.kumar@sprinklr.com>
2022-02-22 10:04:59 +0530

    Friend List toggle view implementation updated

--- script/profile-page.js
+++ script/profile-page.js
@@ -1,12 +1,8 @@
-let button_element = document.querySelector("#view-toggle-button");
+let button_element = document.getElementById("view-toggle-button");
+let list_view_container = document.getElementById("list-view-container");
+let grid_view_container = document.getElementById("grid-view-container");
 let button_icon_element = document.querySelector("#view-toggle-button-icon");
-let list_element = document.querySelector("#sidebar-option-list");
-let list_item_list = document.querySelectorAll(".list-item");
-let friend_article_list = document.querySelectorAll(".friend-entity-grid-view");
-let details_container_list = document.querySelectorAll(".friend-details-container");
-let username_list = document.querySelectorAll(".username");
-let userid_list = document.querySelectorAll(".userid");
-
+list_view_container.style.display = "none";
 
 button_element.addEventListener("click",toggleView);
 
@@ -14,28 +10,14 @@ function toggleView(){
     let value=button_element.value;
     if(value=="grid"){
         button_element.value="list";
-        button_icon_element.innerHTML="apps";
+        button_icon_element.innerHTML="list_alt";
+        grid_view_container.style.display="";
+        list_view_container.style.display="none";
     }
     else{
         button_element.value="grid";
-        button_icon_element.innerHTML="list_alt";
+        button_icon_element.innerHTML="apps";
+        grid_view_container.style.display="none";
+        list_view_container.style.display="";
     }
-    list_element.classList.toggle('list-grid-view');
-    list_item_list.forEach((item)=>{
-        item.classList.toggle('list-item-grid-view');
-    })
-    friend_article_list.forEach((item)=>{
-        item.classList.toggle('friend-entity');
-        item.classList.toggle('friend-entity-grid-view');
-    })
-    details_container_list.forEach((item)=>{
-        item.classList.toggle('friend-details-container-grid-view');
-    });
-    username_list.forEach((item)=>{
-        item.classList.toggle('username-grid-view');
-    });
-    userid_list.forEach((item)=>{
-        item.classList.toggle('userid-grid-view');
-    });
-
 }
